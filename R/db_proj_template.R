1c34a8585d5ecc590d35a82b2a8992b7e1277ab0
ztgroton <ztgroton@gmail.com>
2023-01-29 15:46:37 -0500

    Expanded on S3 Class setup functions for 'db_proj_template'

--- R/db_proj_template.R
+++ R/db_proj_template.R
@@ -1,20 +1,124 @@
 
 #' Constructor for S3 Class 'db_proj_template'
-#'
+#' 
+#' @param type character - Specify database type for template
+#' 
 #' @return S3Class 
 #' @export
 #'
 #' @examples
-#' new_db_proj_template()
-new_db_proj_template <- function() {
+#' \dontrun{
+#' new_db_proj_template('mssql')
+#' }
+new_db_proj_template <- function(type) {
   
+  # Validate Inputs
+  if (missing(type)) {stop("`type` is missing in call to `new_db_proj_template`")}
+  
+  # Initialize Empty S3 Object
   rs <- list(
     data = new.env(), 
-    meta = list()
+    meta = new.env()
   )
   
+  # Set Class
   class(rs) <- c(setdiff('db_proj_template', class(rs)), class(rs))
   
+  # Return S3 Object
   return(rs)
+  
+} 
+
+
+#' Validate that S3 Object is Properly Formatted
+#'
+#' @param obj S3 Object - R Object to be validated against desired format
+#' @param bool TRUE/FALSE - Optionally specify if input 'obj' or logical TRUE/FALSE
+#'
+#' @return R Object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' validate_db_proj_template(obj, TRUE)
+#' validate_db_proj_template(obj, FALSE)
+#' }
+validate_db_proj_template <- function(obj, bool) {
+  
+  # Validate Inputs
+  if (missing(obj)) {stop("`obj` is missing in call to `validate_db_proj_template`")}
+  if (missing(bool)) {bool <- FALSE} 
+  
+  # Initialize Empty Character Vector for Error Messages
+  err <- vector(mode = 'character')
+  
+  # Validate Input Expectations
+  
+  # * `obj`
+  if (!isTRUE(inherits(obj, 'db_proj_template'))) {
+    msg <- "`obj` must inherit from 'db_proj_template'"
+    err <- c(msg, err)
+  }
+  
+  # * `bool` 
+  if (!isTRUE(identical(bool, TRUE)) && !isTRUE(identical(bool, FALSE))) {
+    msg <- "`bool` must be identical with TRUE/FALSE"
+    err <- c(msg, err)
+  }
+  
+  # Conditionally Return Error Messages (if any at this point in execution)
+  if (!isTRUE(length(err) == 0)) {return(err)}
+  
+  # ADD CUSTOM INPUT VALIDATIONS HERE (USE SAME TEMPLATE AS `obj` and `bool`)
+  
+  # Final Output
+  if (!isTRUE(bool)) {
+      
+      if (!isTRUE(length(err) == 0)) {
+          return(obj)
+      } else {
+          return(err)
+      }
+      
+  } else {
+      
+      if (!isTRUE(length(err) == 0)) {
+          return(TRUE)
+      } else {
+          return(FALSE)
+      }
+      
+  }
+  
+} 
+
+
+#' S3 Class Helper Function for 'db_proj_template'
+#'
+#' @param type character - Specify database type for template
+#'
+#' @return S3 Object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' db_proj_template('mssql')
+#' }
+db_proj_template <- function(type) {
+  
+  # Validate Inputs
+  if (missing(type)) {stop("`type` is missing in call to `db_proj_template`")}
+  
+  # Initialize Blank
+  temp <- new_db_proj_template()
+  
+  # Setup Template Metadata
+  setup_db_proj_metadata(temp, type)
+  
+  # Setup Template Data
+  setup_db_proj_data(temp, type)
+  
+  # Validate Template
+  validate_db_proj_template(temp)
   
 }
